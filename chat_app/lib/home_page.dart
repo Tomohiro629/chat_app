a6de605ee42680d09e8a6dd736c6573f18fc1f63
Tomohiro629 <radcell0101@gmail.com>
2022-06-05 22:30:41 +0900

    chat_pageのUI変更

--- chat_app/lib/home_page.dart
+++ chat_app/lib/home_page.dart
@@ -7,24 +7,37 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(actions: const [Text('HomePage')]),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
+            const SizedBox(
+              height: 200,
+            ),
             const Text(
               'Chat App',
-              style: TextStyle(color: Colors.greenAccent, fontSize: 30),
+              style: TextStyle(color: Colors.green, fontSize: 45),
             ),
-            ElevatedButton(
+            const SizedBox(
+              height: 500,
+            ),
+            SizedBox(
+              height: 75.0,
+              width: 200.0,
+              child: ElevatedButton(
                 onPressed: () {
                   Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => ChatPage(),
+                        builder: (context) => const ChatPage(),
                       ));
                 },
-                child: const Text("Start"))
+                child: const Text(
+                  "Start",
+                  style: TextStyle(fontSize: 20.0),
+                ),
+              ),
+            ),
           ],
         ),
       ),

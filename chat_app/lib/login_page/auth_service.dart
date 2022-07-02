dafe905429f888e237bb83a82ed5873901f5ed44
Tomohiro629 <radcell0101@gmail.com>
2022-07-02 18:22:26 +0900

    サインアップ時にユーザーネームの登録を追加

--- chat_app/lib/login_page/auth_service.dart
+++ chat_app/lib/login_page/auth_service.dart
@@ -1,24 +1,47 @@
+import 'package:chat_app/entity/user.dart';
+import 'package:chat_app/repository/user_repository.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final authServiceProvider = ChangeNotifierProvider<AuthService>((ref) {
-  return AuthService();
+  return AuthService(ref.read);
 });
 
 class AuthService extends ChangeNotifier {
-  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  final auth = FirebaseAuth.instance;
+  final Reader _reader;
+  AuthService(this._reader);
 
   Future<String> createUserWithEmailAndPassword(
-      String email, String password, String authName) async {
-    final authResult = await firebaseAuth.createUserWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    final authResult = await auth.createUserWithEmailAndPassword(
       email: email,
       password: password,
     );
     return authResult.user!.uid;
   }
 
-  Future getCurrentUser() async {
-    return firebaseAuth.currentUser;
+  Future<void> addUser({
+    required String userNameText,
+    required String userId,
+  }) async {
+    final user = Users.create(userNameText: userNameText, userId: userId);
+    await _reader(userRepositoryProvider).setUser(user: user);
+  }
+
+  Stream<List<Users>> fetchUsersStream(String userId) {
+    return _reader(userRepositoryProvider).fetchUserStream();
+  }
+
+  Future<void> deleteUser() async {
+    await _reader(userRepositoryProvider).deleteUser();
+  }
+
+  Future<void> logOut() async {
+    await auth.signOut();
+    notifyListeners();
   }
 }

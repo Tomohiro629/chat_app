d970bab15ee5e5a7da01c30c6b6430f2200ed52d
Tomohiro629 <radcell0101@gmail.com>
2022-07-11 21:04:17 +0900

    設定ページにDeleteユーザーを追加した

--- chat_app/lib/setting_page/setting_page.dart
+++ chat_app/lib/setting_page/setting_page.dart
@@ -2,6 +2,7 @@ import 'package:chat_app/home_page/home_page.dart';
 import 'package:chat_app/service/auth_service.dart';
 import 'package:chat_app/service/user_service.dart';
 import 'package:chat_app/setting_page/setting_controller.dart';
+import 'package:chat_app/user_gate/user_gate_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -155,6 +156,71 @@ class SettingPage extends ConsumerWidget {
                                   } catch (e) {
                                     print(e);
                                   }
+                                },
+                              ),
+                              MaterialButton(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(100),
+                                ),
+                                color: const Color.fromARGB(255, 137, 196, 244),
+                                child: const Text("No"),
+                                onPressed: () async {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                            ],
+                          );
+                        });
+                  },
+                ),
+              ),
+              Card(
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(100)),
+                ),
+                child: ListTile(
+                  title: const Text(
+                    "Delete User",
+                  ),
+                  tileColor: const Color.fromARGB(255, 127, 241, 92),
+                  trailing: const Icon(Icons.logout),
+                  leading: ConstrainedBox(
+                    constraints: const BoxConstraints(
+                        minHeight: 44,
+                        minWidth: 34,
+                        maxHeight: 64,
+                        maxWidth: 54),
+                  ),
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(100)),
+                  ),
+                  onTap: () async {
+                    showDialog(
+                        barrierDismissible: false,
+                        context: context,
+                        builder: (childContext) {
+                          return AlertDialog(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(50)),
+                            title: const Text("Delete User!"),
+                            content: const Text("Do you want to delete it?"),
+                            actions: <Widget>[
+                              MaterialButton(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(100),
+                                ),
+                                color: const Color.fromARGB(255, 240, 124, 116),
+                                child: const Text("Yes"),
+                                onPressed: () async {
+                                  await userName.deleteUser(userId);
+
+                                  // ignore: use_build_context_synchronously
+                                  Navigator.pushAndRemoveUntil(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              const UserGatePage()),
+                                      (_) => false);
                                 },
                               ),
                               MaterialButton(

cd37d509bb3968ebb95ecd5ee485badc186dc07c
Tomohiro629 <radcell0101@gmail.com>
2022-07-24 21:29:59 +0900

    ルームセレクトとチャットページのAppBarにアイコン追加

--- chat_app/lib/chat_page/chat_page.dart
+++ chat_app/lib/chat_page/chat_page.dart
@@ -2,8 +2,12 @@ import 'package:chat_app/base_app_bar.dart';
 import 'package:chat_app/chat_page/chat_page_controller.dart';
 import 'package:chat_app/chat_page/message_list_tile.dart';
 import 'package:chat_app/entity/chat_room.dart';
+import 'package:chat_app/entity/chat_user.dart';
 import 'package:chat_app/entity/message.dart';
+import 'package:chat_app/repository/user_repository.dart';
+import 'package:chat_app/service/auth_service.dart';
 import 'package:chat_app/service/coloud_storage_service.dart';
+import 'package:chat_app/service/user_service.dart';
 import 'package:chat_app/setting_page/setting_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -19,12 +23,34 @@ class ChatPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final controller = ref.watch(chatControllerProvider);
+    final userController = ref.watch(userServiceProvider);
+    final userId = ref.watch(authServiceProvider).userId;
     final textEdit = TextEditingController();
 
     return Scaffold(
       appBar: BaseAppBar(
         title: const Text("Chat Room"),
         widgets: [
+          StreamBuilder<List<ChatUser>>(
+              stream: userController.fetchUsersStream(userId),
+              builder: (BuildContext context,
+                  AsyncSnapshot<List<ChatUser>> snapshot) {
+                if (snapshot.hasData) {
+                  return Row(
+                    children: snapshot.data!.map((ChatUser user) {
+                      return CircleAvatar(
+                        foregroundImage: NetworkImage(user.imageURL),
+                      );
+                    }).toList(),
+                  );
+                }
+                if (snapshot.hasError) {
+                  return const Text("error");
+                }
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }),
           IconButton(
             icon: const Icon(Icons.settings),
             onPressed: () {
--- chat_app/lib/room_select/room_select_page.dart
+++ chat_app/lib/room_select/room_select_page.dart
@@ -2,12 +2,11 @@ import 'package:chat_app/base_app_bar.dart';
 import 'package:chat_app/entity/chat_room.dart';
 import 'package:chat_app/add_chat_room/add_chat_room.dart';
 import 'package:chat_app/entity/chat_user.dart';
-import 'package:chat_app/repository/user_repository.dart';
 import 'package:chat_app/room_select/room_list_tile.dart';
 import 'package:chat_app/room_select/room_select_controller.dart';
-import 'package:chat_app/service/coloud_storage_service.dart';
+import 'package:chat_app/service/auth_service.dart';
+import 'package:chat_app/service/user_service.dart';
 import 'package:chat_app/setting_page/setting_page.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutterfire_ui/firestore.dart';
@@ -20,11 +19,33 @@ class RoomSelectPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final controller = ref.watch(roomSelectControllerProvider);
+    final userController = ref.watch(userServiceProvider);
+    final userId = ref.watch(authServiceProvider).userId;
 
     return Scaffold(
       appBar: BaseAppBar(
         title: const Text("Room Select Page"),
         widgets: [
+          StreamBuilder<List<ChatUser>>(
+              stream: userController.fetchUsersStream(userId),
+              builder: (BuildContext context,
+                  AsyncSnapshot<List<ChatUser>> snapshot) {
+                if (snapshot.hasData) {
+                  return Row(
+                    children: snapshot.data!.map((ChatUser user) {
+                      return CircleAvatar(
+                        foregroundImage: NetworkImage(user.imageURL),
+                      );
+                    }).toList(),
+                  );
+                }
+                if (snapshot.hasError) {
+                  return const Text("error");
+                }
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }),
           IconButton(
             icon: const Icon(Icons.settings),
             onPressed: () {

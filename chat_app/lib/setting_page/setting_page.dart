513edb0494870ab768cd6d3e97582f8e96e96159
Tomohiro629 <radcell0101@gmail.com>
2022-07-24 20:12:19 +0900

    DeleteUserCardのwidget分けた

--- chat_app/lib/setting_page/setting_page.dart
+++ chat_app/lib/setting_page/setting_page.dart
@@ -2,6 +2,7 @@ import 'package:chat_app/base_app_bar.dart';
 import 'package:chat_app/home_page/home_page.dart';
 import 'package:chat_app/service/auth_service.dart';
 import 'package:chat_app/service/user_service.dart';
+import 'package:chat_app/setting_page/delete_user_card.dart';
 import 'package:chat_app/setting_page/edit_user_name.dart';
 import 'package:chat_app/setting_page/logout_card.dart';
 import 'package:chat_app/user_gate/user_gate_page.dart';
@@ -30,71 +31,7 @@ class SettingPage extends ConsumerWidget {
               ),
               const LogoutCard(),
               const EditUserNameCard(),
-              Card(
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(100)),
-                ),
-                child: ListTile(
-                  title: const Text(
-                    "Delete User",
-                  ),
-                  tileColor: const Color.fromARGB(255, 127, 241, 92),
-                  trailing: const Icon(Icons.logout),
-                  leading: ConstrainedBox(
-                    constraints: const BoxConstraints(
-                        minHeight: 44,
-                        minWidth: 34,
-                        maxHeight: 64,
-                        maxWidth: 54),
-                  ),
-                  shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(100)),
-                  ),
-                  onTap: () async {
-                    showDialog(
-                        barrierDismissible: false,
-                        context: context,
-                        builder: (childContext) {
-                          return AlertDialog(
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(50)),
-                            title: const Text("Delete User!"),
-                            content: const Text("Do you want to delete it?"),
-                            actions: <Widget>[
-                              MaterialButton(
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(100),
-                                ),
-                                color: const Color.fromARGB(255, 240, 124, 116),
-                                child: const Text("Yes"),
-                                onPressed: () async {
-                                  await userName.deleteUser(userId);
-
-                                  // ignore: use_build_context_synchronously
-                                  Navigator.pushAndRemoveUntil(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) =>
-                                              const UserGatePage()),
-                                      (_) => false);
-                                },
-                              ),
-                              MaterialButton(
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(100),
-                                ),
-                                color: const Color.fromARGB(255, 137, 196, 244),
-                                child: const Text("No"),
-                                onPressed: () async {
-                                  Navigator.of(context).pop();
-                                },
-                              ),
-                            ],
-                          );
-                        });
-                  },
-                ),
-              ),
+              const DeleteUserCard(),
             ],
           ),
         ],

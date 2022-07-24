be54dcfa9ebf005e2d4341c06c539fe53ad7e3c1
Tomohiro629 <radcell0101@gmail.com>
2022-07-24 20:02:19 +0900

    logoutのwidget分けた

--- chat_app/lib/setting_page/setting_page.dart
+++ chat_app/lib/setting_page/setting_page.dart
@@ -2,6 +2,7 @@ import 'package:chat_app/base_app_bar.dart';
 import 'package:chat_app/home_page/home_page.dart';
 import 'package:chat_app/service/auth_service.dart';
 import 'package:chat_app/service/user_service.dart';
+import 'package:chat_app/setting_page/logout_card.dart';
 import 'package:chat_app/user_gate/user_gate_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -13,7 +14,6 @@ class SettingPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final controller = ref.watch(authServiceProvider);
     final userName = ref.watch(userServiceProvider);
     final userId = ref.watch(authServiceProvider).userId;
     final editName = TextEditingController();
@@ -31,70 +31,7 @@ class SettingPage extends ConsumerWidget {
               Container(
                 padding: const EdgeInsets.all(8),
               ),
-              Card(
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(100)),
-                ),
-                child: ListTile(
-                  title: const Text(
-                    "Log Out",
-                  ),
-                  tileColor: const Color.fromARGB(255, 127, 241, 92),
-                  trailing: const Icon(Icons.logout),
-                  leading: ConstrainedBox(
-                    constraints: const BoxConstraints(
-                        minHeight: 44,
-                        minWidth: 34,
-                        maxHeight: 54,
-                        maxWidth: 54),
-                  ),
-                  shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(100)),
-                  ),
-                  onTap: () async {
-                    showDialog(
-                        barrierDismissible: false,
-                        context: context,
-                        builder: (childContext) {
-                          return AlertDialog(
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(50)),
-                            title: const Text("Log Out!"),
-                            content: const Text("Do you want to logout it?"),
-                            actions: <Widget>[
-                              MaterialButton(
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(100),
-                                ),
-                                color: const Color.fromARGB(255, 240, 124, 116),
-                                child: const Text("Yes"),
-                                onPressed: () async {
-                                  await controller.logOut();
-                                  await Navigator.of(context).pushReplacement(
-                                    MaterialPageRoute(
-                                      builder: (context) {
-                                        return const HomePage();
-                                      },
-                                    ),
-                                  );
-                                },
-                              ),
-                              MaterialButton(
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(100),
-                                ),
-                                color: const Color.fromARGB(255, 137, 196, 244),
-                                child: const Text("No"),
-                                onPressed: () async {
-                                  Navigator.of(context).pop();
-                                },
-                              ),
-                            ],
-                          );
-                        });
-                  },
-                ),
-              ),
+              const LogoutCard(),
               Card(
                 shape: const RoundedRectangleBorder(
                   borderRadius: BorderRadius.all(Radius.circular(100)),

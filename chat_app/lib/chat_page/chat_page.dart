dd8bd3e8ec65670fc69601d65a4dcbfe0dcc80a5
Tomohiro629 <radcell0101@gmail.com>
2022-08-02 12:21:10 +0900

    メッセージ入力欄のUI変更

--- chat_app/lib/chat_page/chat_page.dart
+++ chat_app/lib/chat_page/chat_page.dart
@@ -43,7 +43,6 @@ class ChatPage extends ConsumerWidget {
         ],
       ),
       body: Stack(
-        fit: StackFit.expand,
         children: [
           Container(
             padding: const EdgeInsets.only(bottom: 50.0),
@@ -63,63 +62,72 @@ class ChatPage extends ConsumerWidget {
                 }),
           ),
           Align(
-            alignment: Alignment.bottomCenter,
-            child: TextFormField(
-              keyboardType: TextInputType.multiline,
-              maxLines: null,
-              controller: textEdit,
-              decoration: InputDecoration(
-                contentPadding:
-                    const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
-                border: const OutlineInputBorder(),
-                labelStyle: const TextStyle(fontSize: 20),
-                focusColor: Colors.green,
-                // prefixIcon: Row(children: [
-                //   IconButton(
-                //     onPressed: () {
-                //       imagePickerService.takeGallery();
-                //     },
-                //     icon: const Icon(Icons.photo),
-                //     color: const Color.fromARGB(255, 255, 176, 7),
-                //   ),
-                //   IconButton(
-                //     onPressed: () {
-                //       imagePickerService.takeCamera();
-                //     },
-                //     icon: const Icon(Icons.camera_alt),
-                //     color: const Color.fromARGB(255, 255, 176, 7),
-                //   ),
-                // ]),
-                suffixIcon: IconButton(
-                  onPressed: () async {
-                    try {
-                      textEdit.text.isNotEmpty
-                          ? await controller.addMesseage(
-                              messageText: textEdit.text,
-                              chatId: chat.roomId,
-                              // imageURL: storageService.imageURL!,
-                            )
-                          : ScaffoldMessenger.of(context)
-                              .showSnackBar(const SnackBar(
-                              content: Text("Please enter your message"),
-                              backgroundColor: Colors.red,
-                            ));
+            alignment: Alignment.bottomRight,
+            child: Row(
+              children: [
+                IconButton(
+                    onPressed: () {
+                      imagePickerService.takeCamera();
+                    },
+                    icon: const Icon(
+                      Icons.camera_alt,
+                      color: Color.fromARGB(255, 255, 168, 7),
+                    )),
+                IconButton(
+                    onPressed: () {
+                      imagePickerService.takeGallery();
+                    },
+                    icon: const Icon(
+                      Icons.photo,
+                      color: Color.fromARGB(255, 255, 168, 7),
+                    )),
+                SizedBox(
+                  width: 300.0,
+                  child: TextFormField(
+                    keyboardType: TextInputType.multiline,
+                    maxLines: null,
+                    controller: textEdit,
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 5.0, horizontal: 10.0),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                      labelStyle: const TextStyle(fontSize: 20),
+                      focusColor: Colors.green,
+                      suffixIcon: IconButton(
+                        onPressed: () async {
+                          try {
+                            textEdit.text.isNotEmpty
+                                ? await controller.addMesseage(
+                                    messageText: textEdit.text,
+                                    chatId: chat.roomId,
+                                    // imageURL: storageService.imageURL!,
+                                  )
+                                : ScaffoldMessenger.of(context)
+                                    .showSnackBar(const SnackBar(
+                                    content: Text("Please enter your message"),
+                                    backgroundColor: Colors.red,
+                                  ));
 
-                      await controller.addLastMessage(
-                        chatId: chat.roomId,
-                        lastMessage: textEdit.text,
-                      );
-                      await storageService.uploadPostImageAndGetUrl(
-                          file: imagePickerService.imagePath!);
-                      textEdit.clear();
-                    } catch (e) {
-                      print(e);
-                    }
-                  },
-                  icon: const Icon(Icons.send),
+                            await controller.addLastMessage(
+                              chatId: chat.roomId,
+                              lastMessage: textEdit.text,
+                            );
+                            await storageService.uploadPostImageAndGetUrl(
+                                file: imagePickerService.imagePath!);
+                            textEdit.clear();
+                          } catch (e) {
+                            print(e);
+                          }
+                        },
+                        icon: const Icon(Icons.send),
+                      ),
+                    ),
+                    style: const TextStyle(fontSize: 20.0),
+                  ),
                 ),
-              ),
-              style: const TextStyle(fontSize: 20.0),
+              ],
             ),
           ),
         ],

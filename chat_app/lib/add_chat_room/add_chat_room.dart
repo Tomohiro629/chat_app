74d94c72bc1233f0a98a3950deb4e9bef7cc158c
Tomohiro629 <radcell0101@gmail.com>
2022-08-02 11:24:23 +0900

    addChatRoomのダイアログをページで分けた

--- chat_app/lib/add_chat_room/add_chat_room.dart
+++ chat_app/lib/add_chat_room/add_chat_room.dart
@@ -1,7 +1,6 @@
-import 'package:chat_app/add_chat_room/add_chat_room_controller.dart';
+import 'package:chat_app/add_chat_room/search_user_dialog.dart';
 import 'package:chat_app/base_app_bar.dart';
 import 'package:chat_app/repository/user_repository.dart';
-import 'package:chat_app/room_select/room_select_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -18,7 +17,6 @@ class AddChatRoomPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final controller = ref.watch(addChatRoomControllerProvider);
     final userController = ref.watch(userRepositoryProvider);
     final searchUserName = TextEditingController();
 
@@ -75,67 +73,9 @@ class AddChatRoomPage extends ConsumerWidget {
                               barrierDismissible: false,
                               context: context,
                               builder: (childContext) {
-                                return AlertDialog(
-                                  content: Stack(
-                                      alignment: Alignment.center,
-                                      children: [
-                                        CircleAvatar(
-                                          radius: 75.0,
-                                          backgroundColor: Colors.amber[100],
-                                          foregroundImage:
-                                              NetworkImage(user.imageURL),
-                                          child:
-                                              const CircularProgressIndicator(
-                                            backgroundColor:
-                                                Color.fromARGB(255, 3, 104, 7),
-                                          ),
-                                        ),
-                                      ]),
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(50)),
-                                  title: Text(
-                                      "User『 ${user.userName}』 Found! \nChat Start?"),
-                                  actions: <Widget>[
-                                    MaterialButton(
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(100),
-                                      ),
-                                      color: const Color.fromARGB(
-                                          255, 240, 124, 116),
-                                      child: const Text("Start"),
-                                      onPressed: () async {
-                                        controller.setChatRoom(
-                                          lastMessage: "",
-                                          userId: user.userId,
-                                          sendTime: "",
-                                          timeStamp: "",
-                                        );
-
-                                        Navigator.pushAndRemoveUntil(
-                                            context,
-                                            MaterialPageRoute(
-                                                builder: (context) =>
-                                                    RoomSelectPage(
-                                                      chatName:
-                                                          searchUserName.text,
-                                                    )),
-                                            (_) => false);
-                                      },
-                                    ),
-                                    MaterialButton(
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(100),
-                                      ),
-                                      color: const Color.fromARGB(
-                                          255, 137, 196, 244),
-                                      child: const Text("No"),
-                                      onPressed: () async {
-                                        Navigator.of(context).pop();
-                                      },
-                                    ),
-                                  ],
+                                return SearchUserDialog(
+                                  user: user,
+                                  searchUserName: searchUserName.text,
                                 );
                               });
                         } else {

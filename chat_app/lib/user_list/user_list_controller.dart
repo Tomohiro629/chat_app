b02fb25a1bb6ef477aabe955fdc7a4f38413e6b6
Tomohiro629 <radcell0101@gmail.com>
2022-08-04 20:54:19 +0900

    partnerUserのみ取得した

--- chat_app/lib/user_list/user_list_controller.dart
+++ chat_app/lib/user_list/user_list_controller.dart
@@ -1,5 +1,6 @@
 import 'package:chat_app/entity/chat_user.dart';
 import 'package:chat_app/repository/user_repository.dart';
+import 'package:chat_app/service/auth_service.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -13,6 +14,7 @@ class UserListController extends ChangeNotifier {
   UserListController(this._reader);
 
   Query<ChatUser> userListQuery() {
-    return _reader(userRepositoryProvider).userListQuery();
+    return _reader(userRepositoryProvider)
+        .userListQuery(currentUserId: _reader(authServiceProvider).userId);
   }
 }

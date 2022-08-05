a935c0aa4b9af7803973bae1478431130db4c593
Tomohiro629 <radcell0101@gmail.com>
2022-08-05 10:51:18 +0900

    groupSelectPageにログイン中のユーザデータ表示

--- chat_app/lib/group_select/group_select_page.dart
+++ chat_app/lib/group_select/group_select_page.dart
@@ -1,6 +1,9 @@
 import 'package:chat_app/base_app_bar.dart';
 import 'package:chat_app/entity/chat_room.dart';
+import 'package:chat_app/repository/user_repository.dart';
+import 'package:chat_app/room_select/components/current_user_data_dialog.dart';
 import 'package:chat_app/room_select/room_select_controller.dart';
+import 'package:chat_app/service/auth_service.dart';
 import 'package:chat_app/setting_page/setting_page.dart';
 import 'package:chat_app/user_list/user_list_page.dart';
 import 'package:flutter/material.dart';
@@ -12,10 +15,27 @@ class GroupSelectPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final groupSelectController = ref.watch(roomSelectControllerProvider);
+    final userController = ref.watch(userRepositoryProvider);
+
     return Scaffold(
       appBar: BaseAppBar(
         title: const Text("Group Select"),
         widgets: [
+          IconButton(
+            icon: const Icon(Icons.person),
+            onPressed: () async {
+              final user = await userController.getUserDate(
+                  userId: ref.watch(authServiceProvider).userId);
+              showDialog(
+                  barrierDismissible: true,
+                  context: context,
+                  builder: (childContext) {
+                    return CurrentUserDateDialog(
+                      user: user,
+                    );
+                  });
+            },
+          ),
           IconButton(
             icon: const Icon(Icons.settings),
             onPressed: () {

17af1bc30a047a4df6d0ef866f722597d8fdf8d4
Tomohiro629 <radcell0101@gmail.com>
2022-08-08 18:29:24 +0900

    チャットルーム名表示の修正

--- chat_app/lib/add_group/components/add_users_dialog.dart
+++ chat_app/lib/add_group/components/add_users_dialog.dart
@@ -1,7 +1,7 @@
 import 'package:chat_app/add_group/add_user_controller.dart';
-import 'package:chat_app/bottom_navigation_bar/bottom_navigation_bar.dart';
 import 'package:chat_app/entity/chat_room.dart';
 import 'package:chat_app/entity/chat_user.dart';
+import 'package:chat_app/room_select/room_select_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -60,8 +60,9 @@ class AddUserDialog extends ConsumerWidget {
             Navigator.pushAndRemoveUntil(
                 context,
                 MaterialPageRoute(
-                    builder: (context) => BottomNavigationBarPage(
-                          chatName: '',
+                    builder: (context) => RoomSelectPage(
+                          partnerUserName: addUserName,
+                          partnerUserImage: partnerUserImage,
                         )),
                 (_) => false);
           },
